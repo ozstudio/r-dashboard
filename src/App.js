c8614f00239607eb50339665453ff7103748a9ed
OL <ozstudiominigames@gmail.com>
2023-03-28 11:20:17 +0300

    added invoices and contacts screens

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Topbar from '../src/scenes/global/Topbar';
 import Sidebr from './scenes/global/Sidebr';
 import { Route, Routes } from "react-router-dom";
 import Dashboard from './scenes/dashboard';
- //import Team from './scenes/team';
-// import Invoices from './scenes/invoces';
-// import Contacts from './scenes/contacts';
+import Team from './scenes/team';
+import Invoices from './scenes/invoices';
+ import Contacts from './scenes/contacts';
 // import Bar from './scenes/bar';
 // import Form from './scenes/form';
 // import Line from './scenes/line';
@@ -29,9 +29,9 @@ function App() {
             <Topbar/>
             <Routes>
               <Route path="/" element = {<Dashboard/>} />
-              {/* <Route path="/team" element = {<Team/>} /> */}
-              {/* <Route path="/contacts" element = {<Contacts/>} /> */}
-              {/* <Route path="/invoces" element = {<Invoices/>} /> */}
+              <Route path="/team" element = {<Team/>} />
+              <Route path="/contacts" element = {<Contacts/>} />
+              <Route path="/invoices" element = {<Invoices/>} />
               {/* <Route path="/form" element = {<Bar/>} /> */}
               {/* <Route path="pie" element = {<Pie/>} /> */}
               {/* <Route path="/line" element = {<Line/>} /> */}
